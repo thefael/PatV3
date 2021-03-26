863b506543122165fa826d90046121ccfe242a59
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-03-26 07:12:24 -0300

    Create breeds screen with list of breeds

--- PatV2/Breeds/BreedsViewController.swift
+++ PatV2/Breeds/BreedsViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class BreedsViewController: UIViewController {
     let breedsView = BreedsView()
     let service: Service
+    let dataSource = TableViewDataSource<Breed, BreedCell>()
 
     init(service: Service = URLSessionService()) {
         self.service = service
@@ -19,5 +20,36 @@ class BreedsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        configureCell()
+        fetchData()
+    }
+
+    func setupView() {
+        breedsView.tableView.dataSource = dataSource
+        breedsView.tableView.register(BreedCell.self, forCellReuseIdentifier: Constants.cellIdentifier)
+    }
+
+    func configureCell() {
+        dataSource.configureCell = { item, cell in
+            DispatchQueue.main.async {
+                cell.name.text = item.name
+            }
+        }
+    }
+
+    func fetchData() {
+        service.fetchData(from: URL.breedsURL) { (result: Result<[String], Error>) in
+            switch result {
+            case .success(let breedsNames):
+                let breeds = breedsNames.map { name in Breed(name: name) }
+                DispatchQueue.main.async {
+                    self.dataSource.items = breeds
+                    self.breedsView.tableView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
