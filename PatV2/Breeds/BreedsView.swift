f0a5a58e08b21c3cd3df3ce93445912593387f48
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-03-26 07:10:51 -0300

    Create BreedsView

--- PatV2/Breeds/BreedsView.swift
+++ PatV2/Breeds/BreedsView.swift
@@ -1,15 +1,26 @@
 import UIKit
+import SnapKit
 
 class BreedsView: UIView {
 
     let tableView = UITableView()
 
-    override init(frame: CGRect = .zero) {
+    override init(frame: CGRect = UIScreen.main.bounds) {
         super.init(frame: frame)
-        backgroundColor = .purple
+        setupTableView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupTableView() {
+        addSubview(tableView)
+        tableView.snp.makeConstraints { (make) -> Void in
+            make.bottom.equalToSuperview()
+            make.left.equalToSuperview()
+            make.top.equalToSuperview()
+            make.width.equalTo(Constants.screen.width)
+        }
     }
 }
