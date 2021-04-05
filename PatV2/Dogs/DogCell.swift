257f1ce4d7767576768f0d0cc7f151d40ec3f08f
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-04-05 16:42:07 -0300

    Snap dogImageView to the cell content view

--- PatV2/Dogs/DogCell.swift
+++ PatV2/Dogs/DogCell.swift
@@ -1,13 +1,22 @@
 import UIKit
+import SnapKit
 
 class DogCell: UITableViewCell {
-    let dogImage = UIImage()
+    var dogImage = UIImageView()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupDogImage()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupDogImage() {
+        contentView.addSubview(dogImage)
+        dogImage.snp.makeConstraints { (make) -> Void in
+            make.edges.equalToSuperview()
+        }
     }
 }
