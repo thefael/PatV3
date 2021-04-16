86c0695a5416b84120e68c08176c5de2845d4431
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-04-16 16:02:35 -0300

    Add heart images to assets

---
+++ PatV2/Breeds/FavoriteButtonPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteButtonPresenter.swift
+//  PatV2
+//
+//  Created by Rafael Rodrigues on 13/04/21.
+//
+
+import Foundation
---
+++ PatV2/Common/String+CapitalizingFirstLetter.swift
@@ -0,0 +1,8 @@
+//
+//  String+CapitalizingFirstLetter.swift
+//  PatV2
+//
+//  Created by Rafael Rodrigues on 13/04/21.
+//
+
+import Foundation
