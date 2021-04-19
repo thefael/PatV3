565b4563891132aafab05bb31735e64b15bd9e98
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-04-19 16:28:50 -0300

    Create Favorites screen

--- PatV2/Favorites/FavoritesViewController.swift
+++ PatV2/Favorites/FavoritesViewController.swift
@@ -1,8 +1,23 @@
-//
-//  FavoritesViewController.swift
-//  PatV2
-//
-//  Created by Rafael Rodrigues on 16/04/21.
-//
+import UIKit
 
-import Foundation
+class FavoritesViewController: UIViewController {
+    let favoritesView = BreedsView(frame: Constants.screen)
+    let cache = FavoriteBreedsCache.shared
+    let dataSource = TableViewDataSource<String, FavoriteCell>()
+
+    override func loadView() {
+        self.view = favoritesView
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        dataSource.items = cache.cache.sorted { $0 < $1 }
+        favoritesView.tableView.reloadData()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource.configureCell = { item, cell in cell.favoriteLabel.text = item }
+        favoritesView.tableView.dataSource = dataSource
+        favoritesView.tableView.register(FavoriteCell.self, forCellReuseIdentifier: Constants.cellIdentifier)
+    }
+}
