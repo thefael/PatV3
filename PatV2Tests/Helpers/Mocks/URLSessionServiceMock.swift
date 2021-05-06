6ea6abda4c5e01e08e2f136e5ba91a1cc32b5872
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-05-06 17:43:33 -0300

    Create URLSessionServiceMock and SuspendableTaskMock

--- PatV2Tests/Helpers/Mocks/URLSessionServiceMock.swift
+++ PatV2Tests/Helpers/Mocks/URLSessionServiceMock.swift
@@ -1,8 +1,24 @@
-//
-//  URLSessionServiceMock.swift
-//  PatV2Tests
-//
-//  Created by Rafael Rodrigues on 02/05/21.
-//
+@testable import PatV2
+import UIKit
 
-import Foundation
+class URLSessionServiceMock<S>: Service {
+    var fetchDataArgs: (url: URL, completion: (Result<S, Error>) -> Void)?
+    var fetchImageArgs: (url: URL, completion: (Result<UIImage, Error>) -> Void)?
+
+    func fetchData<T>(from url: URL, completion: @escaping ((Result<T, Error>) -> Void)) where T : Decodable {
+        fetchDataArgs = (url: url, completion: completion as! (Result<S, Error>) -> Void)
+    }
+
+    func fetchImage(from url: URL, completion: @escaping ((Result<UIImage, Error>) -> Void)) -> SuspendableTask {
+        fetchImageArgs = (url: url, completion: completion)
+        return SuspendableTaskMock()
+    }
+}
+
+class SuspendableTaskMock: SuspendableTask {
+    var didCallSuspend = false
+
+    func suspend() {
+        didCallSuspend = true
+    }
+}
