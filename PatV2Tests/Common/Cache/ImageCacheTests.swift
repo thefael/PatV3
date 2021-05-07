a5c801832def0ebef2474f056dfb0747003a173b
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-05-07 16:05:11 -0300

    Create ImageCacheTests

--- PatV2Tests/Common/Cache/ImageCacheTests.swift
+++ PatV2Tests/Common/Cache/ImageCacheTests.swift
@@ -1,8 +1,30 @@
-//
-//  ImageCacheTests.swift
-//  PatV2Tests
-//
-//  Created by Rafael Rodrigues on 07/05/21.
-//
+@testable import PatV2
+import XCTest
 
-import Foundation
+class ImageCacheTests: XCTestCase {
+    let cache = NSCache<NSURL, UIImage>()
+    let testImage = ImageCacheSavable.testImage
+    let testNSURL = ImageCacheSavable.testNSURL
+    lazy var imageCache = ImageCache(cache: cache)
+
+    func test_set_shouldSetObject_intoNSCache() {
+        imageCache.set(image: testImage, forKey: testNSURL)
+
+        let savedImage = cache.object(forKey: testNSURL)
+
+        XCTAssertEqual(savedImage, testImage)
+    }
+
+    func test_getImage_shouldReturnCorrectImageForKey() {
+        cache.setObject(testImage, forKey: testNSURL)
+
+        let image = imageCache.getImage(forKey: testNSURL)
+
+        XCTAssertEqual(image, testImage)
+    }
+}
+
+struct ImageCacheSavable {
+    static let testImage = UIImage(systemName: "heart")!
+    static let testNSURL = URL(string: "heart")! as NSURL
+}
