5f38608e38cda527cb7cd3ebeade675b2fe2c4ba
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-05-10 21:34:59 -0300

    Create FavoriteBreedsCacheTests

--- PatV2Tests/Common/Cache/FavoriteBreedsCacheTests.swift
+++ PatV2Tests/Common/Cache/FavoriteBreedsCacheTests.swift
@@ -1,8 +1,39 @@
-//
-//  FavoriteBreedsCacheTests.swift
-//  PatV2Tests
-//
-//  Created by Rafael Rodrigues on 07/05/21.
-//
+@testable import PatV2
+import XCTest
 
-import Foundation
+class FavoriteBreedsCacheTests: XCTestCase {
+    var testCache = [String]()
+    let testBreed = BreedsFixture.breedList[0]
+    let defaults = UserDefaultsAdapterMock()
+    lazy var favoriteBreedsCache = FavoriteBreedsCache(cache: testCache, defaults: defaults)
+
+    func test_put_shouldCallSet() {
+        favoriteBreedsCache.put(breed: testBreed)
+
+        XCTAssert(defaults.didCallSet)
+    }
+
+    func test_put_shouldSetBreedIntoCache() {
+        favoriteBreedsCache.put(breed: testBreed)
+
+        let cache = favoriteBreedsCache.cache
+
+        XCTAssert(cache.contains(testBreed))
+    }
+
+    func test_remove_shouldCallSet() {
+        favoriteBreedsCache.remove(breed: testBreed)
+
+        XCTAssert(defaults.didCallSet)
+    }
+
+    func test_remove_shouldRemoveBreedFromCache() {
+        testCache.append(testBreed)
+        favoriteBreedsCache.cache = testCache
+
+        favoriteBreedsCache.remove(breed: testBreed)
+
+        let cache = favoriteBreedsCache.cache
+        XCTAssertFalse(cache.contains(testBreed))
+    }
+}
